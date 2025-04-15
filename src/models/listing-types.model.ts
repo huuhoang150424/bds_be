d4c3b8435e733ebe4385dd263e7e509c0e3f134c
huuhoang150424 <nguyenhoanghuu15042004@gmail.com>
2025-04-16 00:43:03 +0700

    fix: filter post

--- src/models/listing-types.model.ts
+++ src/models/listing-types.model.ts
@@ -3,10 +3,15 @@ import {
   Column,
   DataType,
   HasMany,
+	AllowNull,
+	Default,
+	BeforeCreate,
+	BeforeUpdate,
 } from 'sequelize-typescript';
 import BaseModel from './base.model';
 import PropertyType from './property-types.model';
 import {  ListingTypes } from './enums';
+import slugify from 'slugify';
 
 
 @Table({ tableName: 'listing_types', timestamps: true })
@@ -14,6 +19,19 @@ export default class ListingType extends BaseModel<string> {
 	@Column(DataType.ENUM(...Object.values(ListingTypes)))
 	listingType !: string;
 
+	@AllowNull(false)
+	@Default('')
+	@Column(DataType.STRING)
+	slug!: string;
+
 	@HasMany(() => PropertyType)
 	propertyType!: PropertyType[];
+
+	@BeforeCreate
+	@BeforeUpdate
+	static generateSlug(instance: ListingType) {
+		if (instance.listingType) {
+			instance.slug = slugify(instance.listingType, { lower: true, strict: true });
+		}
+	}
 }
--- src/models/property-types.model.ts
+++ src/models/property-types.model.ts
@@ -5,27 +5,36 @@ import {
   ForeignKey,
   BelongsTo,
   AllowNull,
+  Default,
+  BeforeCreate,
+  BeforeUpdate,
 } from 'sequelize-typescript';
 
-import {Post,PostDraft,ListingType} from '@models';
+import { Post, PostDraft, ListingType } from '@models';
 import BaseModel from './base.model';
+import slugify from 'slugify';
 
 @Table({ tableName: 'property_types', timestamps: true })
 export default class PropertyType extends BaseModel<string> {
   @AllowNull(false)
   @ForeignKey(() => ListingType)
-  @Column({ type: DataType.UUID})
+  @Column({ type: DataType.UUID })
   listingTypeId!: string;
+
+  @AllowNull(false)
+  @Default('')
+  @Column(DataType.STRING)
+  slug!: string;
 
   @BelongsTo(() => ListingType)
   listingType!: ListingType;
 
   @AllowNull(true)
-	@ForeignKey(() => Post)
+  @ForeignKey(() => Post)
   @Column({ type: DataType.UUID })
   postId!: string;
 
-  @BelongsTo(() => Post,{ onDelete: 'SET NULL' })
+  @BelongsTo(() => Post, { onDelete: 'SET NULL' })
   post!: Post;
 
   @AllowNull(true)
@@ -33,9 +42,17 @@ export default class PropertyType extends BaseModel<string> {
   @Column(DataType.UUID)
   postDraftId!: string;
 
-  @BelongsTo(() => PostDraft,{ onDelete: 'SET NULL' })
+  @BelongsTo(() => PostDraft, { onDelete: 'SET NULL' })
   postDraft!: PostDraft;
 
   @Column({ type: DataType.STRING, allowNull: false })
   name!: string;
+
+  @BeforeCreate
+  @BeforeUpdate
+  static generateSlug(instance: PropertyType) {
+    if (instance.name) {
+      instance.slug = slugify(instance.name, { lower: true, strict: true });
+    }
+  }
 }
